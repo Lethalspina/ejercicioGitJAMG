8c72af49dfda4567069a58c0c4b9de792711d1b2
Lethalspina <jamgubal@gmail.com>
2025-03-04 16:41:35 +0100

    New:PayloadSimulado

--- virus.sh
+++ virus.sh
@@ -2,3 +2,13 @@
 # Mensaje inicial humorístico
 echo "¡Hola! Soy un virus 🦠"
 echo "¡Tu sistema está infectado! 😈"
+
+# -------------------------------
+# Payload Simulado:
+# -------------------------------
+tmp_file="/tmp/infectado.txt"
+echo "Simulación de payload: Creando archivo temporal en $tmp_file"
+touch "$tmp_file"
+for i in {1..10}; do
+    echo "Mensaje aleatorio $RANDOM" >> "$tmp_file"
+done
